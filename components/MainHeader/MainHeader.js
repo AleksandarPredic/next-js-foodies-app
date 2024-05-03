214a18c810c16af768d4b0befaabdb7f45f0bb6f
aleksandarpredic <aleksandar.predic@gmail.com>
2024-05-03 12:39:00 +0200

    header done

--- components/MainHeader/MainHeader.js
+++ components/MainHeader/MainHeader.js
@@ -3,28 +3,33 @@ import styles from './MainHeader.module.css';
 
 import logoImg from '@/assets/logo.png';
 import Image from "next/image";
+import MainHeaderBackground from "@/components/MainHeader/MainHeaderBackground/MainHeaderBackground";
 
 export default function MainHeader() {
   return (
-    <header className={styles.header}>
-      <Link className={styles.logo} href="/">
-        <Image
-          src={logoImg}
-          alt="A plate with food on it"
-          width={80}
-          height={80}
-          priority={true}
-        />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
 
-      <nav className={styles.nav}>
-        <ul style={{color: 'white'}}>
-          <li><Link href="/meals">Browse meals</Link></li>
-          <li><Link href="/community">Foodies Community</Link></li>
-          <li><Link href="/meals/share">Share Meals</Link></li>
-        </ul>
-      </nav>
-    </header>
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image
+            src={logoImg}
+            alt="A plate with food on it"
+            width={80}
+            height={80}
+            priority={true}
+          />
+          NextLevel Food
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul style={{color: 'white'}}>
+            <li><Link href="/meals">Browse meals</Link></li>
+            <li><Link href="/community">Foodies Community</Link></li>
+            <li><Link href="/meals/share">Share Meals</Link></li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
