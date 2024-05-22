460f772eeb93f880ceddf87702de17aa1f523d77
aleksandarpredic <aleksandar.predic@gmail.com>
2024-05-22 15:55:27 +0200

    Started populating meals page + setup SqlLite db

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,21 @@
+import styles from './page.module.css';
+import Link from "next/link";
+import MealsGrid from "@/components/Meals/MealsGrid/MealsGrid";
+
 export default function MealsPage() {
-  return (
-    <main>
-      <h1 style={{color: 'white', textAlign: 'center'}}>
-        Meals
+  return <>
+    <header className={styles.header}>
+      <h1>
+        Delicious meals, created{' '}
+        <span className={styles.highlight}>by you</span>
       </h1>
+      <p>Choose your favorite recipe and cook it yourself.</p>
+      <p className={styles.cta}>
+        <Link href="/meals/share">Share your favorite recipe</Link>
+      </p>
+    </header>
+    <main>
+      <MealsGrid meals={[]} />
     </main>
-  );
+  </>;
 }
---
+++ components/Meals/MealsGrid/MealsGrid.js
@@ -0,0 +1,10 @@
+import styles from './MealsGrid.module.css';
+import MealItem from "@/components/Meals/MealItem/MealItem";
+
+export default function MealsGrid ({meals}) {
+  return <ul className={styles.meals}>
+    {meals.map(meal => <li key={meal.id}>
+      <MealItem {...meal} />
+    </li>)}
+  </ul>;
+}
